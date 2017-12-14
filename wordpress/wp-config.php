c86781ef05dd04d45f7abde3398f1217145b183c
juber sheikh <juber.sheikh@reactiveworks.in>
2017-12-14 11:32:44 +0530

    Update wp-config.php

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -23,10 +23,10 @@
 define('DB_NAME', 'Wordpress');
 
 /** MySQL database username */
-define('DB_USER', 'wpuser');
+define('DB_USER', 'user1');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'juber');
 
 /** MySQL hostname */
 define('DB_HOST', '192.168.1.175');
